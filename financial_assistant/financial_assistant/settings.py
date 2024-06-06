816094b1e5cca7e2b651196b263be48534bcde04
Петр Балдаев <petrbaldaev@MacBook-Air-Petr.local>
2024-06-07 00:23:19 +0300

    create transactions app view and urls

--- financial_assistant/financial_assistant/settings.py
+++ financial_assistant/financial_assistant/settings.py
@@ -30,10 +30,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'financial_assistant.urls'
 
+TEMPLATES_DIR = BASE_DIR / 'templates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,3 +83,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_dev',
+] 
--- financial_assistant/transactions/views.py
+++ financial_assistant/transactions/views.py
@@ -1,3 +1,28 @@
+# Импортируем функцию render()
 from django.shortcuts import render
 
-# Create your views here.
+ice_cream_catalog = [
+    {
+        'id': 0,
+        'title': 'Классический пломбир',
+        'description': 'Настоящее мороженое, '
+                       'для истинных ценителей вкуса. '
+                       'Если на столе появляется пломбир'
+                       ' — это не надолго.',
+    },
+    {
+        'id': 1,
+        'title': 'Мороженое с кузнечиками',
+        'description': 'В колумбийском стиле: мороженое '
+                       'с добавлением настоящих карамелизованных кузнечиков.',
+    },
+    {
+        'id': 2,
+        'title': 'Мороженое со вкусом сыра чеддер',
+        'description': 'Вкус настоящего сыра в вафельном стаканчике.',
+    },
+]
+
+def transactions_list(request):
+    template_name = 'transactions/list.html'
+    return render(request, template_name)

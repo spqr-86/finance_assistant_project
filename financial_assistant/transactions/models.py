1a767748a11b2103ce21393fc162cc91efe4f497
Петр Балдаев <petrbaldaev@MacBook-Air-Petr.local>
2024-06-23 19:03:08 +0300

    add card model

--- financial_assistant/transactions/models.py
+++ financial_assistant/transactions/models.py
@@ -3,10 +3,38 @@ from django.contrib.auth.models import User
 
 from .validators import validate_not_future_date
 
+
+class Card(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    number = models.CharField(
+        max_length=4,
+        unique=True,
+        verbose_name='Номер карты'
+    )
+    expiry_date = models.DateField(
+        verbose_name='Срок действия'
+    )
+    name_on_card = models.CharField(
+        max_length=100,
+        verbose_name='Назание карты'
+    )
+
+    class Meta:
+        verbose_name = 'карта'
+        verbose_name_plural = 'карты'
+
+    def __str__(self):
+        return f"{self.number} - {self.name_on_card}"
+
+
 class Category(models.Model):
     name = models.CharField(
-        max_length=100, 
-        unique=True, 
+        max_length=100,
+        unique=True,
         verbose_name='категория'
     )
 
@@ -20,15 +48,15 @@ class Category(models.Model):
 
 class Currency(models.Model):
     code = models.CharField(
-        max_length=3, 
+        max_length=3,
         unique=True,
-        verbose_name = 'код валюты'
+        verbose_name='код валюты'
     )  # ISO 4217 currency code (e.g., USD, EUR)
     name = models.CharField(
         max_length=50,
-        verbose_name = 'название валюты'
-)
- 
+        verbose_name='название валюты'
+    )
+
     class Meta:
         verbose_name = 'валюта'
         verbose_name_plural = 'валюта'
@@ -39,9 +67,9 @@ class Currency(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(
-        max_length=50, 
+        max_length=50,
         unique=True,
-        verbose_name = 'тег'
+        verbose_name='тег'
     )
 
     class Meta:
@@ -72,40 +100,49 @@ class Transaction(models.Model):
     ]
 
     user = models.ForeignKey(
-        User, 
+        User,
         on_delete=models.CASCADE,
         verbose_name='Пользователь',
-        )
-    date = models.DateField(verbose_name='Дата', validators=[validate_not_future_date])
+    )
+    date = models.DateField(
+        verbose_name='Дата',
+        validators=[validate_not_future_date],
+    )
     description = models.CharField(
         max_length=255,
         verbose_name='Описание',
     )
     category = models.ForeignKey(
-        Category, 
+        Category,
         on_delete=models.CASCADE,
         verbose_name='Категория',
     )
     type = models.CharField(
-        max_length=7, 
+        max_length=7,
         choices=TYPE_CHOICES,
         verbose_name='Тип транзакции',
     )
     amount = models.DecimalField(
-        max_digits=10, 
+        max_digits=10,
         decimal_places=2,
         verbose_name='Сумма',
     )
     currency = models.ForeignKey(
-        Currency, 
-        on_delete=models.SET_NULL, 
+        Currency,
+        on_delete=models.SET_NULL,
         null=True,
         verbose_name='Валюта',
     )
-    
+    card = models.ForeignKey(
+        Card,
+        on_delete=models.CASCADE,
+        verbose_name='Карта',
+        blank=True,
+        null=True,
+    )
     tags = models.ManyToManyField(
-        Tag, 
-        blank=True, 
+        Tag,
+        blank=True,
         verbose_name='Теги',
     )
     notes = models.TextField(
@@ -113,33 +150,32 @@ class Transaction(models.Model):
         verbose_name='Заметки',
     )
     payment_method = models.CharField(
-        max_length=20, 
+        max_length=20,
         choices=PAYMENT_METHOD_CHOICES,
         verbose_name='Способ оплаты',
-        blank=True, 
-        null=True, 
+        blank=True,
+        null=True,
     )
     status = models.CharField(
-        max_length=10, 
-        choices=STATUS_CHOICES, 
+        max_length=10,
+        choices=STATUS_CHOICES,
         default='completed',
         verbose_name='Статус',
-        blank=True, 
-        null=True, 
+        blank=True,
+        null=True,
     )
     is_recurring = models.BooleanField(
         default=False,
         verbose_name='Повторяющаяся транзакция',
-        blank=True, 
-        null=True, 
+        blank=True,
+        null=True,
     )
     recurrence_period = models.CharField(
-        max_length=20, 
-        blank=True, 
-        null=True, 
+        max_length=20,
+        blank=True,
+        null=True,
         verbose_name='Период повторения',
     )  # e.g., 'monthly', 'weekly'
-
 
     class Meta:
         ordering = ['-date']
